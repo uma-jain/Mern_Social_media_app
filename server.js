3678ae1306cf7ffc9c10eded513d03d3ed93ca28
uma jain <umajain1000@gmail.com>
2020-07-18 21:01:36 +0530

    database issue solved

--- server.js
+++ server.js
@@ -39,18 +39,6 @@ if(process.env.NODE_ENV == 'production'){
 
 } 
 
-if(process.env.NODE_ENV == 'production'){
-	//express will serve up production assets
-	//like our main.js file, or main.css file
-   app.use(express.static('client/build'));
-   	//Express will seerve up index.html file
-	//if it doesnt recognize route
-   const path=require("path");
-   app.get("*",(req,res)=>{
-	   res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-   })
-
-} 
 
   //setting port
 app.listen(PORT,function(){console.log("server started at port 5000");})
